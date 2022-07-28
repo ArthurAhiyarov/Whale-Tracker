0b60db485a3770cc46eae19ae9d3905c580f1d4b
Arthur <arakhiyarov@gmail.com>
2022-07-28 17:06:15 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is an app for monitoring huge token transactions on the Etherium network
---
+++ index.js
@@ -0,0 +1,153 @@
+const { ethers, Contract } = require('ethers');
+const player = require('play-sound')((opts = {}));
+
+const rpcURL = 'https://cloudflare-eth.com/';
+const provider = new ethers.providers.JsonRpcProvider(rpcURL);
+
+const CONTRACT_ADDRESS = '0x8D983cb9388EaC77af0474fA441C4815500Cb7BB'; // ATOM
+const CONTRACT_ABI = [
+    {
+        constant: true,
+        inputs: [],
+        name: 'name',
+        outputs: [{ name: '', type: 'string' }],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+    },
+    {
+        constant: false,
+        inputs: [
+            { name: '_spender', type: 'address' },
+            { name: '_value', type: 'uint256' },
+        ],
+        name: 'approve',
+        outputs: [{ name: '', type: 'bool' }],
+        payable: false,
+        stateMutability: 'nonpayable',
+        type: 'function',
+    },
+    {
+        constant: true,
+        inputs: [],
+        name: 'totalSupply',
+        outputs: [{ name: '', type: 'uint256' }],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+    },
+    {
+        constant: false,
+        inputs: [
+            { name: '_from', type: 'address' },
+            { name: '_to', type: 'address' },
+            { name: '_value', type: 'uint256' },
+        ],
+        name: 'transferFrom',
+        outputs: [{ name: '', type: 'bool' }],
+        payable: false,
+        stateMutability: 'nonpayable',
+        type: 'function',
+    },
+    {
+        constant: true,
+        inputs: [],
+        name: 'decimals',
+        outputs: [{ name: '', type: 'uint8' }],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+    },
+    {
+        constant: true,
+        inputs: [{ name: '_owner', type: 'address' }],
+        name: 'balanceOf',
+        outputs: [{ name: 'balance', type: 'uint256' }],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+    },
+    {
+        constant: true,
+        inputs: [],
+        name: 'symbol',
+        outputs: [{ name: '', type: 'string' }],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+    },
+    {
+        constant: false,
+        inputs: [
+            { name: '_to', type: 'address' },
+            { name: '_value', type: 'uint256' },
+        ],
+        name: 'transfer',
+        outputs: [{ name: '', type: 'bool' }],
+        payable: false,
+        stateMutability: 'nonpayable',
+        type: 'function',
+    },
+    {
+        constant: true,
+        inputs: [
+            { name: '_owner', type: 'address' },
+            { name: '_spender', type: 'address' },
+        ],
+        name: 'allowance',
+        outputs: [{ name: '', type: 'uint256' }],
+        payable: false,
+        stateMutability: 'view',
+        type: 'function',
+    },
+    { payable: true, stateMutability: 'payable', type: 'fallback' },
+    {
+        anonymous: false,
+        inputs: [
+            { indexed: true, name: 'owner', type: 'address' },
+            { indexed: true, name: 'spender', type: 'address' },
+            { indexed: false, name: 'value', type: 'uint256' },
+        ],
+        name: 'Approval',
+        type: 'event',
+    },
+    {
+        anonymous: false,
+        inputs: [
+            { indexed: true, name: 'from', type: 'address' },
+            { indexed: true, name: 'to', type: 'address' },
+            { indexed: false, name: 'value', type: 'uint256' },
+        ],
+        name: 'Transfer',
+        type: 'event',
+    },
+];
+let contract = new Contract(CONTRACT_ADDRESS, CONTRACT_ABI, provider);
+
+// ATOM uses 6 decimal places
+const TRANSFER_THRESHOLD = 10000000000; // wei
+
+const playSound = () => {
+    player.play('ding.mp3', function (err) {
+        if (err) throw err;
+    });
+};
+
+const main = async () => {
+    playSound();
+    const name = await contract.name();
+    console.log(
+        `Whale tracker started!\nListening for large transfers on ${name}`
+    );
+
+    contract.on('Transfer', (from, to, amount, data) => {
+        if (amount.toNumber() >= TRANSFER_THRESHOLD) {
+            playSound();
+            console.log(
+                `New whale transfer for ${name}: https://etherscan.io/tx/${data.transactionHash}`
+            );
+        }
+    });
+};
+
+main();
